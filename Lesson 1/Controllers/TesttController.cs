ba917d01b5ec6af7a1347da079a62b6fc5361241
LAPTOP-MNMI5U4M\deman <demanv12@gmail.com>
2023-05-20 10:05:15 +0300

    random array

---
+++ Lesson 1/Controllers/TesttController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesttController : ControllerBase
+    {
+        [HttpGet("{size}")]
+        public IActionResult Get([FromRoute] int size)
+        {
+            //size = Math.Abs(size);
+            if (size < 0)
+            {
+                return BadRequest();
+            }
+
+            var rng = new Random();
+            int[] nums = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                nums[i]=rng.Next(90,790);
+            }
+            return Ok(nums);
+        }
+    }
+}
